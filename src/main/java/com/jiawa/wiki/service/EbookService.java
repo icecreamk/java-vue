301719daf88b831630bdf3a52416c2b84b3892f7
kaixin <kx.lin@postar.cn>
2025-03-25 19:10:42 +0800

    CommonResp

---
+++ src/main/java/com/jiawa/wiki/service/EbookService.java
@@ -0,0 +1,19 @@
+package com.jiawa.wiki.service;
+
+import com.jiawa.wiki.domain.Ebook;
+import com.jiawa.wiki.mapper.EbookMapper;
+import jakarta.annotation.Resource;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class EbookService {
+
+    @Resource
+    private EbookMapper ebookMapper;
+
+    public List<Ebook> list() {
+        return ebookMapper.selectByExample(null);
+    }
+}
