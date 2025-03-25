ea0ca31cbe9eb23f36fae5f1091e23e340a212d8
kaixin <kx.lin@postar.cn>
2025-03-25 19:44:30 +0800

    req

--- src/main/java/com/jiawa/wiki/controller/EbookController.java
+++ src/main/java/com/jiawa/wiki/controller/EbookController.java
@@ -1,9 +1,11 @@
 package com.jiawa.wiki.controller;
 
-import com.jiawa.wiki.domain.Ebook;
+import com.jiawa.wiki.req.EbookQueryReq;
 import com.jiawa.wiki.resp.CommonResp;
 import com.jiawa.wiki.service.EbookService;
 import jakarta.annotation.Resource;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -14,13 +16,14 @@ import java.util.List;
 @RequestMapping("/ebook")
 public class EbookController {
 
+    private static final Logger log = LoggerFactory.getLogger(EbookController.class);
     @Resource
     private EbookService ebookService;
 
     @GetMapping("/list")
-    public CommonResp list() {
-        CommonResp<List<Ebook>> resp = new CommonResp<>();
-        List<Ebook> list = ebookService.list();
+    public CommonResp list(EbookQueryReq req) {
+        CommonResp<List<EbookQueryReq>> resp = new CommonResp<>();
+        List<EbookQueryReq> list = ebookService.list(req);
         resp.setContent(list);
         return resp;
     }
--- src/main/java/com/jiawa/wiki/service/EbookService.java
+++ src/main/java/com/jiawa/wiki/service/EbookService.java
@@ -1,10 +1,14 @@
 package com.jiawa.wiki.service;
 
 import com.jiawa.wiki.domain.Ebook;
+import com.jiawa.wiki.domain.EbookExample;
 import com.jiawa.wiki.mapper.EbookMapper;
+import com.jiawa.wiki.req.EbookQueryReq;
 import jakarta.annotation.Resource;
+import org.springframework.beans.BeanUtils;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
 import java.util.List;
 
 @Service
@@ -13,7 +17,21 @@ public class EbookService {
     @Resource
     private EbookMapper ebookMapper;
 
-    public List<Ebook> list() {
-        return ebookMapper.selectByExample(null);
+    public List<EbookQueryReq> list(EbookQueryReq req) {
+        EbookExample ebookExample = new EbookExample();
+        EbookExample.Criteria criteria = ebookExample.createCriteria();
+        criteria.andNameLike("%" + req.getName() + "%");
+
+        List<Ebook> ebookList = ebookMapper.selectByExample(ebookExample);
+
+        List<EbookQueryReq> respList = new ArrayList<>();
+
+        for (Ebook ebook : ebookList) {
+            EbookQueryReq ebookResp = new EbookQueryReq();
+            BeanUtils.copyProperties(ebook, ebookResp);
+            respList.add(ebookResp);
+        }
+
+        return respList;
     }
 }
