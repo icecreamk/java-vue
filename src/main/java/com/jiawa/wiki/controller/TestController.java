4675172e7d57e40e7cae3da04f652e036238b806
kaixin <kx.lin@postar.cn>
2025-03-25 09:27:13 +0800

    mysql

--- src/main/java/com/jiawa/wiki/controller/TestController.java
+++ src/main/java/com/jiawa/wiki/controller/TestController.java
@@ -1,25 +1,40 @@
-
 package com.jiawa.wiki.controller;
 
+import com.jiawa.wiki.domain.Test;
+import com.jiawa.wiki.service.TestService;
+import jakarta.annotation.Resource;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
+
 @RestController
 public class TestController {
-    @Value("${test.user:TEST_USER}")
-    private String testUser;
+
+    private static final Logger LOG = LoggerFactory.getLogger(TestController.class);
+
+    @Value("${test.hello:TEST}")
+    private String testHello;
+
+    @Resource
+    private TestService testService;
 
     @GetMapping("/hello")
-    public String helloTest() {
-        return "Hello World33: " + testUser;
+    public String hello() {
+        return "Hello World!" + testHello;
     }
 
     @PostMapping("/hello/post")
-    public ResponseEntity<String> helloPost(String name) {
-        return  new ResponseEntity<>("Hello " + name, HttpStatus.OK);
+    public String helloPost(String name) {
+        return "Hello World! Post，" + name;
+    }
+
+    @GetMapping("/test/list")
+    public List<Test> list() {
+        return testService.list();
     }
 }
