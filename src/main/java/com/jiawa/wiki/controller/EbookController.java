0ad67afe801cf0b9fc2df751642cf617c5a26a68
kaixin <kx.lin@postar.cn>
2025-03-30 13:59:29 +0800

    del

--- src/main/java/com/jiawa/wiki/controller/EbookController.java
+++ src/main/java/com/jiawa/wiki/controller/EbookController.java
@@ -1,22 +1,19 @@
 package com.jiawa.wiki.controller;
 
 import com.jiawa.wiki.req.EbookQueryReq;
+import com.jiawa.wiki.req.EbookSaveReq;
 import com.jiawa.wiki.resp.CommonResp;
 import com.jiawa.wiki.resp.EbookQueryResp;
 import com.jiawa.wiki.resp.PageResp;
 import com.jiawa.wiki.service.EbookService;
+import org.springframework.web.bind.annotation.*;
+
 import jakarta.annotation.Resource;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/ebook")
 public class EbookController {
 
-    private static final Logger log = LoggerFactory.getLogger(EbookController.class);
     @Resource
     private EbookService ebookService;
 
@@ -25,6 +22,20 @@ public class EbookController {
         CommonResp<PageResp<EbookQueryResp>> resp = new CommonResp<>();
         PageResp<EbookQueryResp> list = ebookService.list(req);
         resp.setContent(list);
+        return resp;
+    }
+
+    @PostMapping("/save")
+    public CommonResp save(@RequestBody EbookSaveReq req) {
+        CommonResp resp = new CommonResp<>();
+        ebookService.save(req);
+        return resp;
+    }
+
+    @DeleteMapping("/delete/{id}")
+    public CommonResp delete(@PathVariable Long id) {
+        CommonResp resp = new CommonResp<>();
+        ebookService.delete(id);
         return resp;
     }
 }
