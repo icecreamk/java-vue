1a7f6793067cce27a93d63dcb9e268264ff420f9
kaixin <kx.lin@postar.cn>
2025-03-30 14:49:41 +0800

    catagory

--- doc/all.sql
+++ doc/all.sql
@@ -1,34 +1,62 @@
-drop table  if exists `test`;
-create table  `test` (
-                         `id` bigint not null  comment 'id',
-                         `name` varchar(50) comment '名称',
-                         `password` varchar(50) comment  '密码',
-                         primary key (`id`)
-) engine=innodb default charset=utf8mb4 comment='测试';
+drop table if exists `test`;
+create table `test`
+(
+    `id`       bigint not null comment 'id',
+    `name`     varchar(50) comment '名称',
+    `password` varchar(50) comment '密码',
+    primary key (`id`)
+) engine = innodb
+  default charset = utf8mb4 comment ='测试';
 
-insert into `test` (id, name, password) values (2 ,'测试', '111');
+insert into `test` (id, name, password)
+values (2, '测试', '111');
 
-drop table  if exists `demo`;
-create table  `demo` (
-                         `id` bigint not null  comment 'id',
-                         `name` varchar(50) comment '名称',
-                         primary key (`id`)
-) engine=innodb default charset=utf8mb4 comment='测试';
+drop table if exists `demo`;
+create table `demo`
+(
+    `id`   bigint not null comment 'id',
+    `name` varchar(50) comment '名称',
+    primary key (`id`)
+) engine = innodb
+  default charset = utf8mb4 comment ='测试';
 
 
-drop table  if exists  `ebook`;
-create table  `ebook` (
-                          `id` bigint not null  comment 'id',
-                          `name` varchar(50) comment '名称',
-                          `category1_id` bigint comment '分类1',
-                          `category2_id` bigint comment '分类2',
-                          `description` varchar(200) comment '描述',
-                          `cover` varchar(200) comment '封面',
-                          `doc_count` varchar(200) comment '文档数',
-                          `view_count` varchar(200) comment '阅读数',
-                          `vote_count` varchar(200) comment '点赞数',
-                          primary key (`id`)
-) engine=innodb default charset=utf8mb4 comment='电子书';
+drop table if exists `ebook`;
+create table `ebook`
+(
+    `id`           bigint not null comment 'id',
+    `name`         varchar(50) comment '名称',
+    `category1_id` bigint comment '分类1',
+    `category2_id` bigint comment '分类2',
+    `description`  varchar(200) comment '描述',
+    `cover`        varchar(200) comment '封面',
+    `doc_count`    varchar(200) comment '文档数',
+    `view_count`   varchar(200) comment '阅读数',
+    `vote_count`   varchar(200) comment '点赞数',
+    primary key (`id`)
+) engine = innodb
+  default charset = utf8mb4 comment ='电子书';
 
-insert into `ebook` (id, name, description) values (1, 'java', '基础入门');
-insert into `ebook` (id, name, description) values (2, 'vue', '基础3.x入门');
+insert into `ebook` (id, name, description)
+values (1, 'java', '基础入门');
+insert into `ebook` (id, name, description)
+values (2, 'vue', '基础3.x入门');
+
+
+drop table if exists `category`;
+create table `category`
+(
+    `id`     bigint      not null comment 'id',
+    `parent` bigint      not null default 0 comment '父id',
+    `name`   varchar(50) not null comment '名称',
+    `sort`   int comment '顺序',
+    primary key (`id`)
+) engine = innodb
+  default charset = utf8mb4 comment ='分类';
+
+
+insert into `category` (id, parent, name, sort) values (100, 000, '前端开发', 100);
+insert into `category` (id, parent, name, sort) values (101, 100, 'vue', 101);
+insert into `category` (id, parent, name, sort) values (200, 000, '后端', 200);
+insert into `category` (id, parent, name, sort) values (201, 200, 'java', 201);
+insert into `category` (id, parent, name, sort) values (202, 200, 'go', 202);
