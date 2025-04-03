42b24c296d8fc7cc8aa0861f7e98bb89198ca6d2
kx.lin <kx.lin@postar.cn>
2025-04-03 12:17:52 +0800

    login token

--- src/main/java/com/jiawa/wiki/req/UserLoginReq.java
+++ src/main/java/com/jiawa/wiki/req/UserLoginReq.java
@@ -1,21 +1,18 @@
-package com.jiawa.wiki.domain;
+package com.jiawa.wiki.req;
 
-public class User {
-    private Long id;
 
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.Pattern;
+
+public class UserLoginReq {
+
+    @NotEmpty(message = "【用户名】不能为空")
     private String loginName;
 
-    private String name;
-
+    @NotEmpty(message = "【密码】不能为空")
+    // @Length(min = 6, max = 20, message = "【密码】6~20位")
+    @Pattern(regexp = "^(?![0-9]+$)(?![a-zA-Z]+$)[0-9A-Za-z]{6,32}$", message = "【密码】规则不正确")
     private String password;
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
 
     public String getLoginName() {
         return loginName;
@@ -23,14 +20,6 @@ public class User {
 
     public void setLoginName(String loginName) {
         this.loginName = loginName;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
     }
 
     public String getPassword() {
@@ -47,9 +36,7 @@ public class User {
         sb.append(getClass().getSimpleName());
         sb.append(" [");
         sb.append("Hash = ").append(hashCode());
-        sb.append(", id=").append(id);
         sb.append(", loginName=").append(loginName);
-        sb.append(", name=").append(name);
         sb.append(", password=").append(password);
         sb.append("]");
         return sb.toString();
--- src/main/java/com/jiawa/wiki/req/UserResetPasswordReq.java
+++ src/main/java/com/jiawa/wiki/req/UserResetPasswordReq.java
@@ -1,12 +1,15 @@
-package com.jiawa.wiki.domain;
+package com.jiawa.wiki.req;
 
-public class User {
+
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Pattern;
+
+public class UserResetPasswordReq {
     private Long id;
 
-    private String loginName;
-
-    private String name;
-
+    @NotNull(message = "【密码】不能为空")
+    // @Length(min = 6, max = 20, message = "【密码】6~20位")
+    @Pattern(regexp = "^(?![0-9]+$)(?![a-zA-Z]+$)[0-9A-Za-z]{6,32}$", message = "【密码】至少包含 数字和英文，长度6-32")
     private String password;
 
     public Long getId() {
@@ -15,22 +18,6 @@ public class User {
 
     public void setId(Long id) {
         this.id = id;
-    }
-
-    public String getLoginName() {
-        return loginName;
-    }
-
-    public void setLoginName(String loginName) {
-        this.loginName = loginName;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
     }
 
     public String getPassword() {
@@ -48,8 +35,6 @@ public class User {
         sb.append(" [");
         sb.append("Hash = ").append(hashCode());
         sb.append(", id=").append(id);
-        sb.append(", loginName=").append(loginName);
-        sb.append(", name=").append(name);
         sb.append(", password=").append(password);
         sb.append("]");
         return sb.toString();
